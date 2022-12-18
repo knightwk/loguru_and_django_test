88da45bc60b93599cc93a2468b753a43ae9d02e1
knight <wk_knight@163.com>
2022-12-18 21:21:33 +0800

    init setting

---
+++ django_and_loguru_test/urls.py
@@ -0,0 +1,38 @@
+"""django_and_loguru_test URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from random import randint
+
+from django.contrib import admin
+from django.http import JsonResponse
+from django.urls import path
+
+from django_and_loguru_test.settings import my_logger
+
+
+def index(request):
+    for i in range(1000):
+        rand_num = randint(1, 10)
+        if i % rand_num == 0:
+            my_logger.info(f"This is an info msg. num = {i}, rand_num = {rand_num}.")
+        else:
+            my_logger.error(f"This is an info msg. num = {i}, rand_num = {rand_num}.")
+    return JsonResponse({"code": 200, "msg": "success", "result": [0, 1, 2]})
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("index", index)
+]
